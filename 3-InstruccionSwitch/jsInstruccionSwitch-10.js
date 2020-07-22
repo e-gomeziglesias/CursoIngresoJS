4674244fc9ebae0fba2a9b9aca2dcd6060473f0b
e-gomeziglesias <68024973+e-gomeziglesias@users.noreply.github.com>
2020-07-22 14:04:25 -0300

    Ejercicio 10 switch

--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,68 @@
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
-
+  //declaro variables
+  let destino;
+  let estacion;
+  
+  //cargo los datos del desplegable en la variables
+  
+  destino = document.getElementById("txtIdDestino").value;
+  estacion = document.getElementById("txtIdEstacion").value;
+  
+//planteo el switch anidado
+switch(estacion)
+{
+	case "Invierno":
+		switch(destino)
+		{
+			case "Bariloche":
+			  alert("Se viaja.");
+			  break;
+			  
+			case "Mar del plata":
+			case "Cataratas":
+			case "Cordoba":
+			  alert("No se viaja.");
+			  break;
+		}
+	break;
+	case "Verano":
+		switch(estacion)
+		{
+			case "Mar del plata":
+			case "Cataratas":
+			  alert("Se viaja.");
+			  break;
+			case "Bariloche":
+			case "Cordoba":
+			  alert("No se viaja.");
+			  break;
+		}
+	break;
+	case "Otoño":
+		switch(estacion)
+		{
+			case "Bariloche":
+			case "Mar del plata":
+			case "Cataratas":
+			case "Cordoba":
+			  alert("Se viaja.");
+			  break;
+		}
+	break;
+	case "Primavera":
+		switch(estacion)
+		{
+			case "Bariloche":
+			  alert("No se viaja.");
+			  break;
+			case "Cataratas":
+			case "Mar del plata":
+			case "Cordoba":
+			  alert("Se viaja.");
+			  break;
+		}
+	break;
+}
+  
 }//FIN DE LA FUNCIÓN
