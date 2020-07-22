029bcdfb76204059a79fde12087aa0a27baaec31
e-gomeziglesias <68024973+e-gomeziglesias@users.noreply.github.com>
2020-07-22 13:31:41 -0300

    Ejercicio 09 switch

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -4,49 +4,79 @@ function mostrar()
 let destino;
 let precioBase = 15000;
 let descuento;
+let aumento;
 let precioFinal;
+
+// Inicializo descuento y aumento
+descuento =0 ;
+aumento = 0 ;
 //asigno el valor del cuadro de texto a la variable destino
 destino = document.getElementById("txtIdDestino").value;
-
-//planteo el switch
+estacion = document.getElementById("txtIdEstacion").value;
+//planteo el switch anidado
 switch(destino)
 {
 	case "Bariloche":
 		switch(estacion)
 		{
 			case "Verano":
-			case "Otoño":
+			  descuento = 20/100;
+			  break;
+			  
 			case "Invierno":
+			  aumento = 20/100;
+			  break;
+			  
+			case "Otoño":
 			case "Primavera":
+			  aumento = 10/100;
 		}
 	break;
 	case "Cataratas":
 		switch(estacion)
 		{
 			case "Verano":
-			case "Otoño":
+			  aumento = 10/100;
+			  break;
 			case "Invierno":
+			  descuento = 10/100;
+			  break;
+			case "Otoño":
 			case "Primavera":
+			  aumento = 10/100;
+			  break;
 		}
 	break;
 	case "Mar del plata":
 		switch(estacion)
 		{
 			case "Verano":
-			case "Otoño":
+			  aumento = 20/100;
+			  break;
 			case "Invierno":
+			  descuento = 20/100;
+			  break;
+			case "Otoño":
 			case "Primavera":
+			  aumento = 10/100;
+			  break;
 		}
 	break;
-	case "Ushuaia":
+	case "Cordoba":
 		switch(estacion)
 		{
 			case "Verano":
-			case "Otoño":
+			  aumento = 10/100;
+			  break;
 			case "Invierno":
-			case "Primavera":
+			  descuento = 10/100;
+			  break;
 		}
 	break;
 }
+//calculo el precio final
+
+precioFinal = precioBase + precioBase*aumento - precioBase*descuento;
+alert("El precio final de su viaje es de $"+precioFinal);
 
 }//FIN DE LA FUNCIÓN
